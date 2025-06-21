05fe408509324be67b56bba45fe520678fb05723
Paul Garcia <pbrain19@gmail.com>
2025-06-21 17:14:02 -0500

    Update promptTemplate function in util.tsx to enhance response evaluation format. Introduce structured XML-like tags for goal, context, and additional information to improve clarity and guidance for users.

--- src/components/util.tsx
+++ src/components/util.tsx
@@ -6,17 +6,23 @@ import {
 } from "../types";
 
 export const promptTemplate = (tasker_opinion: string, context: string) => `
+
+<goal>
 I need you to help me evaluate the response of an tecnical expert. 
 Do you agree with the following preference opinion: ${tasker_opinion}?
+</goal>
 
-Context: ${context}
+<context>
+${context}
+</context>
 
-
+<additional_info>
 Try to focus on functionality, correctness, and technical correctness.
 
 If there is a minor improvement just rewrite the preference opinion.
 
 You may need to doublecheck online resources for certain documentation references for specific technoligies.
+</additional_info>
 
 `;
 
